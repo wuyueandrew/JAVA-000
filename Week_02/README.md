3a59f4393a79070ead5ff78481993c7b5220fc16
武越 <wuyue_andrew@outlook.com>
2020-10-28 21:11:44 +0800

    HW02

--- Week_02/README.md
+++ Week_02/README.md
@@ -1,1 +1,9 @@
 学习笔记
+
+jvm调优经验：
+https://www.yuque.com/docs/share/142a863a-dab2-46cd-96fe-a6520bcc033b?# 《调优经验》
+
+GC补了一篇脑图：
+https://www.yuque.com/docs/share/c5203501-a858-4c18-a1ae-84d9c55ba1b8?# 《GC》
+
+时间太赶了，倒回去看了两遍视频，本来想自学下Arthas结果也没来得及，下次会补上。
---
+++ Week_02/httpclient/HttpService.java
@@ -0,0 +1,34 @@
+import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+
+public class HttpService {
+
+    public static void main(String[] args) throws Exception {
+        try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            final HttpGet httpget = new HttpGet("http://localhost:8088/api/hello");
+            final HttpClientResponseHandler<String> responseHandler = response -> {
+                final int status = response.getCode();
+                if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
+                    final HttpEntity entity = response.getEntity();
+                    try {
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } catch (final ParseException ex) {
+                        throw new ClientProtocolException(ex);
+                    }
+                } else {
+                    throw new ClientProtocolException("Unexpected response status: " + status);
+                }
+            };
+            final String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println(responseBody);
+        }
+    }
+}
